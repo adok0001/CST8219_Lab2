df033750cf1cd9f9f85fdca2b6ea279740a56039
amarring <adok0001@algonquinlive.com>
2021-05-30 21:05:50 -0700

    On branch master Initial commit Changes to be committed: 	new file:   Game.h 	new file:   Lab2.cpp

---
+++ Game.h
@@ -0,0 +1,56 @@
+﻿/**
+* Name: Adokeme Tamara
+* Student Number: 040873188
+**/
+
+#pragma once
+#include <iostream>
+using namespace std;
+const double NO_TIMEOUT(-1);
+
+
+namespace CST8219 {
+	class Game {
+	private:
+		//Class private properties declaration
+		string name;
+		int numPlayers;
+		double timeout = NO_TIMEOUT;
+
+	public:
+		//Class constructors declaration
+		Game(string gName, int nPlayers, double duration) {
+			this->name = gName;
+			this->numPlayers = nPlayers;
+			this->timeout = duration;
+			//cout << "In constructor with 3 parameters" << endl;
+		}
+		Game(string gName, int nPlayers) {
+			this->name = gName;
+			this->numPlayers = nPlayers;
+			this->timeout = NO_TIMEOUT;
+			//cout << "In constructor with 2 parameters, nPlayers= " << nPlayers << " Name - " << gName << endl;
+		}
+		Game(string gName) {
+			this->name = gName;
+			//Game(gName, 1);
+			//cout << "In constructor with 1 parameter. Name: " << gName << " | nPlayers: " << numPlayers << " | Timeout: " << timeout << endl;
+		}
+		//default
+		Game() {
+			this->name = "[Noname]";
+			this->timeout = NO_TIMEOUT;
+			this->numPlayers = 1;
+			//cout << "In constructor with 0 parameters. Name: " << name << " | nPlayers: " << numPlayers << " | Timeout: " << timeout << endl;
+		}
+
+		/*~Game() {
+			cout << "In destructor" << endl;
+		}*/
+		void printGame();
+		void createOneGame();
+		
+
+	};
+}
+
---
+++ Lab2.cpp
@@ -0,0 +1,91 @@
+﻿/**
+* Name: Adokeme Tamara
+* Student Number: 040873188
+**/
+
+#include <iostream>
+#include "Game.h"
+
+using namespace std;
+using namespace CST8219;
+
+int lab1() {
+	//Game game1("Tam", 12);
+	//Game myGame();
+	cout << "I made a game\n\n" << endl;
+	return EXIT_SUCCESS;
+}
+
+int lab2() {
+	Game game0;
+	Game game1("Game1");
+	Game game2("Game2", 4);
+	Game game3("Game3", 4, 2000);
+
+	cout << "\nGame0 takes " << sizeof(game0) << endl;
+	cout << "Game1 takes " << sizeof(game1) << endl;
+	cout << "Game2 takes " << sizeof(game2) << endl;
+	cout << "Game3 takes " << sizeof(game3) << endl;
+
+	game0.printGame();
+	game1.printGame();
+	game2.printGame();
+	game3.printGame();
+
+	game0.createOneGame();
+
+	return EXIT_SUCCESS;
+}
+
+
+void Game::printGame() {
+	cout << "Name: " << name << " - Numplayers: " << numPlayers << " - Timeout: " << timeout << endl;
+}
+
+void Game::createOneGame() {
+	
+	while (true) {
+		cout << "Enter the name: " << endl;
+		cin >> name;
+		if (name.empty()) {
+			cout << "Name cannot be empty" << endl;
+			cin.clear();
+		}
+		else break;
+	}
+
+	while (true) {
+		cout << "Enter the number of players" << endl;
+		cin >> numPlayers;
+		if (numPlayers < 1 || numPlayers > 10) {
+			cin.clear();
+			cout << "Number of players must be between 1-10 " << endl;
+		}
+		else
+			break;
+	}
+	while (true) {
+		cout << "Enter the game duration" << endl;
+		cin >> timeout;
+		if (timeout <= 0) {
+			cin.clear();
+			cout << "Timeout cannot be a negative number\n";
+		}
+		else 
+			break;
+
+	}
+
+	Game aGame = Game(name, numPlayers, timeout);
+	aGame.printGame();
+}
+
+
+int main(int argc, char** argv)
+{
+	lab1();
+	lab2();
+	
+
+	return EXIT_SUCCESS;
+}
