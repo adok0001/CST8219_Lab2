a692e448cdda24b6ccd4e3b1e777dee7f0b2b47e
amarring <adok0001@algonquinlive.com>
2021-05-31 00:30:10 -0700

    Creating lab functions

--- Game.h
+++ Game.h
@@ -3,54 +3,36 @@
 * Student Number: 040873188
 **/
 
-#pragma once
 #include <iostream>
 using namespace std;
 const double NO_TIMEOUT(-1);
 
-
 namespace CST8219 {
 	class Game {
 	private:
-		//Class private properties declaration
-		string name;
 		int numPlayers;
-		double timeout = NO_TIMEOUT;
+		double timeout;
 
 	public:
-		//Class constructors declaration
-		Game(string gName, int nPlayers, double duration) {
-			this->name = gName;
-			this->numPlayers = nPlayers;
-			this->timeout = duration;
-			//cout << "In constructor with 3 parameters" << endl;
+		Game(int nPlayers, double duration) {
+			numPlayers = nPlayers;
+			timeout = duration;
+			cout << "In constructor with 2 parameters" << endl;
 		}
-		Game(string gName, int nPlayers) {
-			this->name = gName;
-			this->numPlayers = nPlayers;
-			this->timeout = NO_TIMEOUT;
-			//cout << "In constructor with 2 parameters, nPlayers= " << nPlayers << " Name - " << gName << endl;
+		Game(int nPlayers) {
+			timeout = NO_TIMEOUT;
+			cout << "In constructor with 1 parameter, nPlayers= " << endl;
 		}
-		Game(string gName) {
-			this->name = gName;
-			//Game(gName, 1);
-			//cout << "In constructor with 1 parameter. Name: " << gName << " | nPlayers: " << numPlayers << " | Timeout: " << timeout << endl;
-		}
-		//default
 		Game() {
-			this->name = "[Noname]";
-			this->timeout = NO_TIMEOUT;
-			this->numPlayers = 1;
-			//cout << "In constructor with 0 parameters. Name: " << name << " | nPlayers: " << numPlayers << " | Timeout: " << timeout << endl;
+			numPlayers = 1;
+			timeout = NO_TIMEOUT;
+			cout << "In constructor with 0 parameters" << endl;
 		}
-
-		/*~Game() {
+		~Game() {
 			cout << "In destructor" << endl;
-		}*/
-		void printGame();
-		void createOneGame();
-		
-
+		}
 	};
 }
+
+
 
